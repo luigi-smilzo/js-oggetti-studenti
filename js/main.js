8d7c93019c394d0fed62765b8d672f5330e56b93
Luigi <luigismilzo@live.com>
2020-04-23 16:31:56 +0200

    Log:  key / value

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    var student = {
+        firstName: 'Luigi',
+        lastName: 'Smilzo',
+        age: 30
+    }
+
+    for (var key in student) {
+        console.log('Chiave:', key, '/', 'Valore:', student[key]);
+    }
+
+
+
+
+
+
+}); // <-- End ready
